33d7159159f9ed726ca4e0ea64d07e1db3ab20a7
Jorge Pinell <jpinell1075@gmail.com>
2022-02-26 00:14:24 -0600

    Configuraciones iniciales

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        //Constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //Esto se hace por cada entidad a mapear en la BD
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
